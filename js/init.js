77c12543187f28e692eb05db006e51684612e6ef
oteroweb <oterolopez1990@gmail.com>
2016-09-08 12:20:13 -0400

    arreglo

--- js/init.js
+++ js/init.js
@@ -6,3 +6,10 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+  $('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 500);
+    return false;
+});
